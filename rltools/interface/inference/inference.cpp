50b684caff9ba8908fbd209b2f5280dcd1493d8f
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-10-04 13:36:24 -0400

    fixing inference (todo: switch to evaluate_step and expose the policy state)

--- rltools/interface/inference/inference.cpp
+++ rltools/interface/inference/inference.cpp
@@ -8,7 +8,7 @@
 #include <rl_tools/nn/layers/sample_and_squash/operations_generic.h>
 #include <rl_tools/nn_models/mlp_unconditional_stddev/operations_generic.h>
 #include <rl_tools/nn_models/mlp/operations_generic.h>
-#include <rl_tools/nn_models/sequential/operations_generic.h>
+#include <rl_tools/nn_models/sequential_v2/operations_generic.h>
 
 #include <pybind11/pybind11.h>
 #include <pybind11/functional.h>
@@ -21,6 +21,7 @@ namespace rlt = rl_tools;
 using DEVICE = rlt::devices::DEVICE_FACTORY<>;
 
 DEVICE device;
+auto rng = rlt::random::default_engine(typename DEVICE::SPEC::RANDOM{});
 using MODEL_TYPE = decltype(policy::module);
 using T = typename MODEL_TYPE::T;
 using TI = typename DEVICE::index_t;
@@ -41,29 +42,29 @@ pybind11::array_t<T> evaluate(const pybind11::array_t<T>& input){
     }
     auto input_data_ptr = static_cast<T*>(input_info.ptr);
     size_t num_elements = input_info.shape[0];
-    if(num_elements != MODEL_TYPE::INPUT_DIM){
+    if(num_elements != MODEL_TYPE::INPUT_SHAPE::GET<2>){
         throw std::runtime_error("Incompatible input dimension. Check the dimension of the input and the one configured when building the RLtools interface");
     }
-    rlt::MatrixStatic<rlt::matrix::Specification<T, TI, 1, MODEL_TYPE::INPUT_DIM>> input_rlt;
-    rlt::malloc(device, input_rlt);
+    rlt::Tensor<rlt::tensor::Specification<T, TI, MODEL_TYPE::INPUT_SHAPE, false>> input_rlt;
+    static_assert(MODEL_TYPE::INPUT_SHAPE::LENGTH == 3);
+    static_assert(MODEL_TYPE::INPUT_SHAPE::GET<0> == 1);
+    static_assert(MODEL_TYPE::INPUT_SHAPE::GET<1> == 1);
     for(TI input_i=0; input_i<num_elements; input_i++){
-        rlt::set(input_rlt, 0, input_i, input_data_ptr[input_i]);
+        rlt::set(device, input_rlt, input_data_ptr[input_i], 0, 0, input_i);
     }
-    rlt::MatrixStatic<rlt::matrix::Specification<T, TI, 1, MODEL_TYPE::OUTPUT_DIM>> output_rlt;
-    rlt::malloc(device, output_rlt);
-    bool rng = false;
+    rlt::Tensor<rlt::tensor::Specification<T, TI, MODEL_TYPE::OUTPUT_SHAPE, false>> output_rlt;
+    static_assert(MODEL_TYPE::OUTPUT_SHAPE::LENGTH == 3);
+    static_assert(MODEL_TYPE::OUTPUT_SHAPE::GET<0> == 1);
+    static_assert(MODEL_TYPE::OUTPUT_SHAPE::GET<1> == 1);
     rlt::evaluate(device, policy::module, input_rlt, output_rlt, buffer, rng);
 
-    std::vector<T> output(MODEL_TYPE::OUTPUT_DIM);
+    std::vector<T> output(MODEL_TYPE::OUTPUT_SHAPE::GET<2>);
 
-    for (TI output_i = 0; output_i < MODEL_TYPE::OUTPUT_DIM; output_i++) {
-        output[output_i] = rlt::get(output_rlt, 0, output_i);
+    for (TI output_i = 0; output_i < MODEL_TYPE::OUTPUT_SHAPE::GET<2>; output_i++) {
+        output[output_i] = rlt::get(device, output_rlt, 0, 0, output_i);
     }
 
-    rlt::free(device, input_rlt);
-    rlt::free(device, output_rlt);
-
-    return pybind11::array_t<T>(MODEL_TYPE::OUTPUT_DIM, output.data());
+    return pybind11::array_t<T>(MODEL_TYPE::OUTPUT_SHAPE::GET<2>, output.data());
 }
 
 PYBIND11_MODULE(RL_TOOLS_MODULE_NAME, m){
--- rltools/interface/training/training.cpp
+++ rltools/interface/training/training.cpp
@@ -104,11 +104,12 @@ namespace RL_TOOLS_MODULE_NAME{
     using LOOP_STATE = typename LOOP_CONFIG::template State<LOOP_CONFIG>;
 
 
-    // constexpr TI EVALUATION_BATCH_SIZE = 1;
-    // using EVALUATION_ACTOR_TEMP = decltype(get_actor(std::declval<LOOP_STATE>()))::template CHANGE_BATCH_SIZE<EVALUATION_BATCH_SIZE>;
-    // using EVALUATION_ACTOR = decltype(get_actor(std::declval<LOOP_STATE>()))::template CHANGE_CAPABILITY<rlt::nn::layer_capability::Forward<>>;
-    // using EVALUATION_ACTOR_STATE = EVALUATION_ACTOR::State<>;
-    // using EVALUATION_ACTOR_BUFFER = EVALUATION_ACTOR::Buffer<>;
+    constexpr TI EVALUATION_BATCH_SIZE = 1;
+    using ACTOR_TYPE = rlt::utils::typing::remove_reference_t<decltype(rlt::get_actor(std::declval<LOOP_STATE&>()))>;
+    using EVALUATION_ACTOR_TEMP = ACTOR_TYPE::template CHANGE_BATCH_SIZE<TI, EVALUATION_BATCH_SIZE>;
+    using EVALUATION_ACTOR = EVALUATION_ACTOR_TEMP::template CHANGE_CAPABILITY<rlt::nn::layer_capability::Forward<>>;
+    using EVALUATION_ACTOR_STATE = EVALUATION_ACTOR::State<>;
+    using EVALUATION_ACTOR_BUFFER = EVALUATION_ACTOR::Buffer<>;
 
     #ifdef RL_TOOLS_USE_PYTHON_ENVIRONMENT
     void set_environment_factory(std::function<pybind11::object()> p_environment_factory){
@@ -120,62 +121,63 @@ namespace RL_TOOLS_MODULE_NAME{
     }
     #endif
     struct State: LOOP_STATE{
-        // EVALUATION_ACTOR evaluation_actor;
-        // EVALUATION_ACTOR_STATE evaluation_actor_state;
-        // EVALUATION_ACTOR_BUFFER evaluation_actor_buffer;
+        EVALUATION_ACTOR evaluation_actor;
+        EVALUATION_ACTOR_STATE evaluation_actor_state;
+        EVALUATION_ACTOR_BUFFER evaluation_actor_buffer;
         bool evaluation_actor_synced = false;
         State(TI seed){
             rlt::malloc(device, static_cast<LOOP_STATE&>(*this));
             rlt::init(device, static_cast<LOOP_STATE&>(*this), seed);
-            // rlt::malloc(device, evaluation_actor);
-            // rlt::malloc(device, evaluation_actor_state);
-            // rlt::malloc(device, evaluation_actor_buffer);
+            rlt::malloc(device, evaluation_actor);
+            rlt::malloc(device, evaluation_actor_state);
+            rlt::malloc(device, evaluation_actor_buffer);
             evaluation_actor_synced = false;
         }
         bool step(){
             evaluation_actor_synced = false;
             return rlt::step(device, static_cast<LOOP_STATE&>(*this));
         }
-        // pybind11::array_t<T> action(const pybind11::array_t<T>& observation){
-        //     if(!evaluation_actor_synced){
-        //         rlt::copy(device, device, rlt::get_actor(*this), evaluation_actor);
-        //         evaluation_actor_synced = true;
-        //     }
-        //     pybind11::buffer_info observation_info = observation.request();
-        //     if (observation_info.format != pybind11::format_descriptor<T>::format() || observation_info.ndim != 1) {
-        //         throw std::runtime_error("Incompatible buffer format. Check the floating point type of the observation returned by env.step() and the one configured when building the RLtools interface");
-        //     }
-        //     auto observation_data_ptr = static_cast<T*>(observation_info.ptr);
-        //     size_t num_elements = observation_info.shape[0];
-        //     if(num_elements != ENVIRONMENT::Observation::DIM){
-        //         throw std::runtime_error("Incompatible observation dimension. Check the dimension of the observation returned by env.step() and the one configured when building the RLtools interface");
-        //     }
-        //     rlt::Tensor<rlt::tensor::Specification<T, TI, rlt::tensor::Shape<TI, EVALUATION_BATCH_SIZE, ENVIRONMENT::Observation::DIM>, true>> observation_rlt;
-        //     rlt::malloc(device, observation_rlt);
-        //     for(TI observation_i=0; observation_i<num_elements; observation_i++){
-        //         rlt::set(device, observation_rlt, observation_data_ptr[observation_i], 0, observation_i);
-        //     }
-        //     // using ACTOR_TYPE = rlt::utils::typing::remove_reference<decltype(rlt::get_actor(*this))>::type;
-        //     rlt::Tensor<rlt::tensor::Specification<T, TI, rlt::tensor::Shape<TI, EVALUATION_BATCH_SIZE, ENVIRONMENT::ACTION_DIM>, true>> action_rlt; //2x for mean and std
-        //     rlt::malloc(device, action_distribution);
-        //     bool rng = false;
-        //     rlt::Mode<rlt::mode::Evaluation<>> mode;
-        //     rlt::evaluate_step(device, evaluation_actor, observation_rlt, evaluation_actor_state, action_rlt, evaluation_actor_buffer, rng, mode);
-        //     rlt::free(device, observation_rlt);
+        pybind11::array_t<T> action(const pybind11::array_t<T>& observation){
+            if(!evaluation_actor_synced){
+                rlt::copy(device, device, rlt::get_actor(*this), evaluation_actor);
+                evaluation_actor_synced = true;
+            }
+            pybind11::buffer_info observation_info = observation.request();
+            if (observation_info.format != pybind11::format_descriptor<T>::format() || observation_info.ndim != 1) {
+                throw std::runtime_error("Incompatible buffer format. Check the floating point type of the observation returned by env.step() and the one configured when building the RLtools interface");
+            }
+            auto observation_data_ptr = static_cast<T*>(observation_info.ptr);
+            size_t num_elements = observation_info.shape[0];
+            if(num_elements != ENVIRONMENT::Observation::DIM){
+                throw std::runtime_error("Incompatible observation dimension. Check the dimension of the observation returned by env.step() and the one configured when building the RLtools interface");
+            }
+            rlt::Tensor<rlt::tensor::Specification<T, TI, rlt::tensor::Shape<TI, EVALUATION_BATCH_SIZE, ENVIRONMENT::Observation::DIM>, false>> observation_rlt;
+            for(TI observation_i=0; observation_i<num_elements; observation_i++){
+                rlt::set(device, observation_rlt, observation_data_ptr[observation_i], 0, observation_i);
+            }
+            // using ACTOR_TYPE = rlt::utils::typing::remove_reference<decltype(rlt::get_actor(*this))>::type;
+            rlt::Tensor<rlt::tensor::Specification<T, TI, rlt::tensor::Shape<TI, EVALUATION_BATCH_SIZE, ENVIRONMENT::ACTION_DIM>, false>> action_rlt; //2x for mean and std
+            bool rng = false;
+            rlt::Mode<rlt::mode::Evaluation<>> mode;
+            rlt::evaluate_step(device, evaluation_actor, observation_rlt, evaluation_actor_state, action_rlt, evaluation_actor_buffer, rng, mode);
 
-        //     std::vector<T> action(ENVIRONMENT::ACTION_DIM);
+            std::vector<T> action(ENVIRONMENT::ACTION_DIM);
 
-        //     for (TI action_i = 0; action_i < ENVIRONMENT::ACTION_DIM; action_i++){
-        //         action[action_i] = rlt::get(device, action_rlt, 0, action_i);
-        //     }
+            for (TI action_i = 0; action_i < ENVIRONMENT::ACTION_DIM; action_i++){
+                action[action_i] = rlt::get(device, action_rlt, 0, action_i);
+            }
 
-        //     return pybind11::array_t<T>(ENVIRONMENT::ACTION_DIM, action.data());
-        // }
-        // std::string export_policy(){
-        //     return rlt::save_code(device, rlt::get_actor(*this), "policy");
-        // }
+            return pybind11::array_t<T>(ENVIRONMENT::ACTION_DIM, action.data());
+        }
+        std::string export_policy(){
+            rlt::copy(device, device, rlt::get_actor(*this), evaluation_actor);
+            return rlt::save_code(device, evaluation_actor, "policy");
+        }
         ~State(){
             rlt::free(device, static_cast<LOOP_STATE&>(*this));
+            rlt::malloc(device, evaluation_actor);
+            rlt::malloc(device, evaluation_actor_state);
+            rlt::malloc(device, evaluation_actor_buffer);
         }
     };
 }
@@ -187,9 +189,9 @@ PYBIND11_MODULE(RL_TOOLS_MODULE_NAME, m){
     m.doc() = "RLtools Training Loop";
     pybind11::class_<RL_TOOLS_MODULE_NAME::State>(m, "State")
             .def(pybind11::init<RL_TOOLS_MODULE_NAME::TI>())
-            .def("step", &RL_TOOLS_MODULE_NAME::State::step, "Step the loop");
-            // .def("action", &RL_TOOLS_MODULE_NAME::State::action, "Get the action for the given observation")
-            // .def("export_policy", &RL_TOOLS_MODULE_NAME::State::export_policy, "Export the policy to a python file");
+            .def("step", &RL_TOOLS_MODULE_NAME::State::step, "Step the loop")
+            .def("action", &RL_TOOLS_MODULE_NAME::State::action, "Get the action for the given observation")
+            .def("export_policy", &RL_TOOLS_MODULE_NAME::State::export_policy, "Export the policy to a python file");
 #ifdef RL_TOOLS_USE_PYTHON_ENVIRONMENT
     m.def("set_environment_factory", &RL_TOOLS_MODULE_NAME::set_environment_factory, "Set the environment factory");
 #endif
